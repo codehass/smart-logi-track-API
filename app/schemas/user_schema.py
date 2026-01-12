62d61f7cea7539b64a4a5a92ff8a08b8d51c0a8f
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-01-12 10:04:23 +0100

    create user schemas

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,7 +1,28 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, ConfigDict, Field
+from typing import Literal
+from typing import List
 
 
 class UserBase(BaseModel):
     username: str
     email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserSchema(UserBase):
+    id: int
+    is_active: bool
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class TokenSchema(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str
