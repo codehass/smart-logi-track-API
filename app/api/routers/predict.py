a90d4071767f922025a7afb731e972c8b2adf5ad
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-01-15 22:07:29 +0100

    create predict endpoints

--- app/api/routers/predict.py
+++ app/api/routers/predict.py
@@ -1,27 +1,49 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from ...schemas.user_schema import TrajectFeaturesRequest
 from ml.scripts.predictor import TripPredictor
+from ...db.database import get_db
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from ...models.user_model import EtaPrediction
 
 router = APIRouter(prefix="/api/v1/predict", tags=["Prediction routes"])
 
 
 @router.post("/")
-def predict(traject: TrajectFeaturesRequest):
+def predict(traject: TrajectFeaturesRequest, db: Session = Depends(get_db)):
     predictor = TripPredictor()
-
     result = predictor.predict(traject)
+    new_prediction = EtaPrediction(predicted_trip_duration=result)
+    db.add(new_prediction)
+    db.commit()
+    db.refresh(new_prediction)
 
     return {"estimated_duration": result}
 
 
-# {
-#     "trip_distance": 1.6,
-#     "fare_amount": 10.0,
-#     "tip_amount": 3.0,
-#     "tolls_amount": 0.0,
-#     "total_amount": 18.0,
-#     "Airport_fee": 0.0,
-#     "RatecodeID": 1,
-#     "pickuphour": 0,
-#     "dayof_week": 4,
-# }
+@router.get("/analytics/avg-duration-by-hour")
+def get_avg_duration_by_hour(db: Session = Depends(get_db)):
+    query = text(
+        """
+        SELECT pickuphour, AVG(trip_duration) AS avg_trip_duration
+        FROM silver_data
+        GROUP BY pickuphour
+        ORDER BY pickuphour
+    """
+    )
+    result = db.execute(query).mappings().all()
+    return result
+
+
+@router.get("/analytics/payment-analysis")
+def get_avg_duration_by_hour(db: Session = Depends(get_db)):
+    query = text(
+        """
+        SELECT payment_type, COUNT(*) AS total_trips, AVG(trip_duration) AS 
+        avg_duration 
+        FROM silver_data 
+        GROUP BY payment_type
+    """
+    )
+    result = db.execute(query).mappings().all()
+    return result
