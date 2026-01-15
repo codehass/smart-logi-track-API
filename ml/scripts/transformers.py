b81bd7a4f144b0116504b766c9db272ba47eb0be
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-01-15 11:28:12 +0100

    create function to save data

--- ml/scripts/transformers.py
+++ ml/scripts/transformers.py
@@ -1,18 +1,42 @@
+import os
+import shutil
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import current_timestamp, input_file_name
 
 
-def load_and_save_bronze(input_path, output_path):
+def save_as_single_parquet(df, output_filename):
+    """
+    Saves a Spark DataFrame as a single parquet file with a specific name.
+    """
+    temp_dir = "temp_spark_out"
+
+    df.coalesce(1).write.mode("overwrite").parquet(temp_dir)
+
+    part_file = [
+        f
+        for f in os.listdir(temp_dir)
+        if f.startswith("part-") and f.endswith(".parquet")
+    ][0]
+
+    if os.path.exists(output_filename):
+        os.remove(output_filename)
+
+    shutil.move(os.path.join(temp_dir, part_file), output_filename)
+
+    shutil.rmtree(temp_dir)
+
+    print(f"Saved: {output_filename}")
+
+
+def load_and_save_bronze(input_path, output_filename):
     spark = SparkSession.builder.getOrCreate()
 
     df = spark.read.format("parquet").load(input_path)
-
-    from pyspark.sql.functions import current_timestamp, input_file_name
 
     df_bronze = df.withColumn("ingestion_timestamp", current_timestamp()).withColumn(
         "source_file", input_file_name()
     )
 
-    df_bronze.coalesce(1).write.mode("overwrite").format("parquet").save(output_path)
+    save_as_single_parquet(df_bronze, output_filename)
 
     return df_bronze
